a2b55ea02a1a8706f77e8fd067b4415d17930f64
Alec Keeler <alec.keeler@gmail.com>
2022-12-15 13:07:46 -0600

    auth me phases complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,58 @@
 const router = require('express').Router();
 
-router.post('/test', function (req, res) {
-    res.json({ requestBody: req.body });
-});
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+const { restoreUser } = require('../../utils/auth.js');
+
+router.use(restoreUser);
+// Routers should be attached below here
+// IE for a path of /api/feature...
+// router.use('/feature', featureRouter)
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+
+
+
+
+
+// TESTING vvvvvv
+
+// router.post('/test', function (req, res) {
+//     res.json({ requestBody: req.body });
+// });
+
+// GET /api/set-token-cookie
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         }
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user: user });
+// });
+
+// GET /api/restore-user
+// router.get(
+//     '/restore-user',
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
 
 module.exports = router;
